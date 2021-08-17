54fb60acc649dd7035f99d60865561daca1362b2
Courtney Jones <courtney.jones@e-gineering.com>
2021-08-17 19:38:47 +0000

    Add api for foods. Manage state and effects.

--- eg-react-app/src/App.tsx
+++ eg-react-app/src/App.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from "react";
+import { getFoods, deleteFood } from "./api/foodsApi";
+
 type Food = {
+  id: number;
   name: string;
   qty: number;
   minQty: number;
@@ -8,34 +12,59 @@ type Food = {
 // Exercize 2:
 // 1. Add reorderPoint (number), type (string)
 // 2. Display in a table.
-const foods: Food[] = [
-  { name: "carrot", qty: 1, minQty: 1, type: "veggie" },
-  { name: "potato", qty: 2, minQty: 1, type: "veggie" },
-];
 
 export function App() {
-  function renderFood(food: Food) {
-    return (
-      <tr key={food.name}>
-        <td>{food.name}</td>
-        <td>{food.qty}</td>
-        <td>{food.minQty}</td>
-        <td>{food.type}</td>
-      </tr>
-    );
-  }
+  const [foods, setFoods] = useState<Food[]>([]);
+
+  useEffect(() => {
+    async function callGetFoods() {
+      const response = await getFoods();
+      if (!response.ok) throw new Error("Call to getFoods() failed.");
+      const json = await response.json();
+      setFoods(json);
+    }
+    callGetFoods();
+  }, []); // use empty array for useEffect dependencies so it will only run once
 
   return (
     <>
       <h1>Cojo's Pantry</h1>
+
       <table>
         <thead>
-          <th>Name</th>
-          <th>Qty</th>
-          <th>Reorder Point</th>
-          <th>Type</th>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Qty</th>
+            <th>Reorder Point</th>
+            <th>Type</th>
+          </tr>
         </thead>
-        <tbody>{foods.map((food) => renderFood(food))}</tbody>
+        <tbody>
+          {/* Add delete button next to name. When clicked, alert('clicked') */}
+          {foods.map((food) => {
+            return (
+              <tr key={food.name}>
+                <td>
+                  <input
+                    type="button"
+                    onClick={async () => {
+                      await deleteFood(food.id);
+                      // remove deleted food from state
+                      const newFoods = foods.filter((f) => f.id !== food.id);
+                      setFoods(newFoods);
+                    }}
+                    value="Delete"
+                  />
+                </td>
+                <td>{food.name}</td>
+                <td>{food.qty}</td>
+                <td>{food.minQty}</td>
+                <td>{food.type}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </>
   );
