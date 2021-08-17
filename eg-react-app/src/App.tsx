d733361abe074078d394a3b8624252903583538f
Courtney Jones <courtney.jones@e-gineering.com>
2021-08-17 17:07:15 +0000

    Create list of foods.

--- eg-react-app/src/App.tsx
+++ eg-react-app/src/App.tsx
@@ -1,4 +1,25 @@
 
+type Food = {
+	name: string;
+	qty: number;
+}
+
+const foods: Food[] = [
+	{ name: 'carrot', qty: 1 },
+	{ name: 'potato', qty: 2 }
+];
+
 export function App() {
-	return <h1>Cojo's Pantry</h1>
+	function renderFoods() {
+		return foods.map(food => <li>{ food.name }</li>);
+	}
+
+	return (
+		<>
+			<h1>Cojo's Pantry</h1>
+			<ul>
+				{ renderFoods() }
+			</ul>
+		</>
+	)
 }
